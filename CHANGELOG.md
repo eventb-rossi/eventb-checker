840818bfe49f425f98b7f66042554d2ecfe3d196
Denis Efremov <efremov@linux.com>
2026-06-13 21:13:55 +0400

    Release v1.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## [1.6] - 2026-06-13
 
 ### Added
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.eventb"
-version = "1.5"
+version = "1.6"
 val projectVersion = version.toString()
 
 repositories {
